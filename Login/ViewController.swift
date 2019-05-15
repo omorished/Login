5224396548a9b5cabb945f84f5863336a090a50c
osamah morished <omorished@gmail.com>
2019-05-15 08:18:21 +0300

    create Login label

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Os! on 15/05/2019.
+//  Copyright © 2019 Os!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
